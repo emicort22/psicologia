58db37646c3cfc4c46e4e48f64c31f368fe18bd3
maria emilia <mecortegozo@gmail.com>
2023-11-23 10:14:12 -0300

    construccion

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { findByPlaceholderText } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Licenciada en Psicología
+       
+        
+         Maria Emilia Cortegozo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a className='sitio'>
+         sitio en construcción
+        </a> 
+        <p >
+<a  href="https://wa.me/543512130222">
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/240px-WhatsApp.svg.png" width="50" height="50"/></a>
+</p>
+Consultas | turnos
       </header>
     </div>
   );
