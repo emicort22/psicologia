e904e0749c68873f633e6d31c5be3d48a5bb93fb
maria emilia <mecortegozo@gmail.com>
2023-11-23 11:00:28 -0300

    mensaje

--- src/App.js
+++ src/App.js
@@ -13,15 +13,20 @@ function App() {
         
          Maria Emilia Cortegozo
         </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <a className='sitio'>
-         sitio en construcción
-        </a> 
-        <p >
+
+      
+        Consultas | turnos
+
 <a  href="https://wa.me/543512130222">
 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/240px-WhatsApp.svg.png" width="50" height="50"/></a>
-</p>
-Consultas | turnos
+
+        <div className='sitio'>
+          
+
+        <img src={logo} className="App-logo" alt="logo" />
+         sitio en construcción
+        </div> 
+      
       </header>
     </div>
   );
